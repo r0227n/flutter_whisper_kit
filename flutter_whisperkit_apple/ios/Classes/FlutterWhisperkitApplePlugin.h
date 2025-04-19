87376aab1ba39f0a1bda5605985e62826db6b2f6
r0227n <developryo@gmail.com>
2025-04-19 09:16:00 +0900

    delete: flutter_whisperkit_apple directory

--- flutter_whisperkit_apple/ios/Classes/FlutterWhisperkitApplePlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FlutterWhisperkitApplePlugin : NSObject<FlutterPlugin>
-@end 
--- flutter_whisperkit_apple/macos/Classes/FlutterWhisperkitApplePlugin.h
+++
@@ -1,4 +0,0 @@
-#import <FlutterMacOS/FlutterMacOS.h>
-
-@interface FlutterWhisperkitApplePlugin : NSObject<FlutterPlugin>
-@end 
