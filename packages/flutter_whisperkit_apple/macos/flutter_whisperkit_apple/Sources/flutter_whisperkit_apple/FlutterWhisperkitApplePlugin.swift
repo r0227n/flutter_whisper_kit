99a4db6f5f71bc3d18fb7627c32b937e9d8b31f0
r0227n <developryo@gmail.com>
2025-04-20 11:39:55 +0900

    refactor: call piegon class

--- packages/flutter_whisperkit_apple/macos/flutter_whisperkit_apple/Sources/flutter_whisperkit_apple/FlutterWhisperkitApplePlugin.swift
+++ packages/flutter_whisperkit_apple/macos/flutter_whisperkit_apple/Sources/flutter_whisperkit_apple/FlutterWhisperkitApplePlugin.swift
@@ -2,33 +2,29 @@ import Cocoa
 import FlutterMacOS
 import WhisperKit
 
-public class FlutterWhisperkitApplePlugin: NSObject, FlutterPlugin {
-  private var whisperKit: WhisperKit?
-  
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_whisperkit_apple", binaryMessenger: registrar.messenger)
-    let instance = FlutterWhisperkitApplePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
-    case "createWhisperKit":
-      Task {
+private class WhisperKitApiImpl: WhisperKitMessage {
+  func getPlatformVersion(completion: @escaping (Result<String?, Error>) -> Void) {
+    completion(.success("macOS " + ProcessInfo.processInfo.operatingSystemVersionString))
+  }
+  
+  func createWhisperKit(model: String?, modelRepo: String?, completion: @escaping (Result<String?, Error>) -> Void) {
+    Task {
         do {
-          let args = call.arguments as? [String: Any]
-          let model = args?["model"] as? String
-          let modelRepo = args?["modelRepo"] as? String
+
+          let pipe = try? await WhisperKit()
      
-          result("WhisperKit instance created successfully: \(model) \(modelRepo)")
+          completion(.success("WhisperKit instance created successfully: \(model ?? "default") \(modelRepo ?? "default")"))
         } catch {
-          result(FlutterError(code: "WHISPERKIT_INIT_ERROR", message: error.localizedDescription, details: nil))
+          completion(.failure(error))
         }
       }
-    default:
-      result(FlutterMethodNotImplemented)
-    }
+  }
+}
+
+public class FlutterWhisperkitApplePlugin: NSObject, FlutterPlugin {
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    // Pigeonで生成されたSetupコードを呼び出す
+    WhisperKitMessageSetup.setUp(binaryMessenger: registrar.messenger, api: WhisperKitApiImpl())
   }
 }
