cb9ac68117e9a6292568908f63ccde39b884c681
r0227n <developryo@gmail.com>
2025-04-29 20:16:07 +0900

    add: dependencies add flutter_whisperkit

--- packages/flutter_whisperkit_apple/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/flutter_whisperkit_apple/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_whisperkit
 import flutter_whisperkit_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterWhisperkitPlugin.register(with: registry.registrar(forPlugin: "FlutterWhisperkitPlugin"))
   FlutterWhisperkitApplePlugin.register(with: registry.registrar(forPlugin: "FlutterWhisperkitApplePlugin"))
 }
