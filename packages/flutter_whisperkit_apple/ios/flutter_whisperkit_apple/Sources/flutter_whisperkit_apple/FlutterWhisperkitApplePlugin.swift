15cb64232fe094e3630e95fdf5a69ec6a9fddc93
r0227n <developryo@gmail.com>
2025-04-29 20:19:45 +0900

    delete: unused function

--- packages/flutter_whisperkit_apple/ios/flutter_whisperkit_apple/Sources/flutter_whisperkit_apple/FlutterWhisperkitApplePlugin.swift
+++ packages/flutter_whisperkit_apple/ios/flutter_whisperkit_apple/Sources/flutter_whisperkit_apple/FlutterWhisperkitApplePlugin.swift
@@ -13,16 +13,6 @@ private let transcriptionStreamChannelName = "flutter_whisperkit_apple/transcrip
 private class TranscriptionStreamHandler: NSObject, FlutterStreamHandler {
   private var eventSink: FlutterEventSink?
   
-  func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-    eventSink = events
-    return nil
-  }
-  
-  func onCancel(withArguments arguments: Any?) -> FlutterError? {
-    eventSink = nil
-    return nil
-  }
-  
   func sendTranscription(_ result: TranscriptionResult?) {
     if let eventSink = eventSink {
       DispatchQueue.main.async {
