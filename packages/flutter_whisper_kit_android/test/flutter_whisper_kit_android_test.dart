744f0d17d926ca2d4bef668106d117ec58bcd575
r0227n <developryo@gmail.com>
2025-07-19 10:12:48 +0900

    revert: flutter_whisper_kit_android directory delete

--- packages/flutter_whisper_kit_android/test/flutter_whisper_kit_android_test.dart
+++
@@ -1,8 +0,0 @@
-/// F.I.R.S.T. Principles Test for flutter_whisper_kit_android Package Structure
-///
-/// F - Fast: < 0.1 seconds execution
-/// I - Independent: No dependencies between tests
-/// R - Repeatable: Consistent results across environments
-/// S - Self-validating: Clear pass/fail outcomes
-/// T - Timely: Written before implementation (Red phase)
-void main() {}
